983b4207da0a334eb45b9d466f38422dc5cdc29e
filipfabris1 <filip.fabris@fer.hr>
2022-04-20 11:09:47 +0200

    first commit

---
+++ javascript/order.js
@@ -0,0 +1,9 @@
+
+let input = document.getElementsByClassName("cookie-banner")
+function hide(){
+    console.log("clicked")
+    console.log(input);
+    input[0].style.visibility = 'hidden';
+    
+}
+
